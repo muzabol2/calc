6734fa332e294d9d7b891970ae0d02799b56530f
Bolesław Muzalewski <bolekbilly@gmail.com>
2024-01-10 08:11:53 +0100

    Prettier files

--- src/script.js
+++ src/script.js
@@ -1,54 +1,58 @@
-import Calculator from "./Calculator.js"
+import Calculator from "./Calculator.js";
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const signButton = document.querySelector('[data-plusminus-sign]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
-const toggleButton = document.querySelector('[data-theme-toggle]');
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const signButton = document.querySelector("[data-plusminus-sign]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+const toggleButton = document.querySelector("[data-theme-toggle]");
 
 const calculator = new Calculator(
-   previousOperandTextElement,
-   currentOperandTextElement
-)
+  previousOperandTextElement,
+  currentOperandTextElement
+);
 
-numberButtons.forEach(button => {
-   button.addEventListener('click', () => {
-      calculator.appendNumber(button.innerText)
-      calculator.updateDisplay()
-   })
-})
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-operationButtons.forEach(button => {
-   button.addEventListener('click', () => {
-      calculator.chooseOperation(button.innerText)
-      calculator.updateDisplay()
-   })
-})
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-equalsButton.addEventListener('click', () => {
-   calculator.compute()
-   calculator.updateDisplay()
-})
+equalsButton.addEventListener("click", () => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
 
-signButton.addEventListener('click', () => {
-   calculator.appendSign()
-   calculator.updateDisplay()
-})
+signButton.addEventListener("click", () => {
+  calculator.appendSign();
+  calculator.updateDisplay();
+});
 
-allClearButton.addEventListener('click', () => {
-   calculator.clear()
-   calculator.updateDisplay()
-})
+allClearButton.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
 
-deleteButton.addEventListener('click', () => {
-   calculator.delete()
-   calculator.updateDisplay()
-})
+deleteButton.addEventListener("click", () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
 
-toggleButton.addEventListener('click', () => {
-   document.body.classList.toggle('light-mode');
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
 });
